d609a3b8a0167d691d250f692b6cc93b5667a572
alexlasota <alexylasota@gmail.com>
2024-08-20 20:59:34 +0200

    hi

---
+++ src/main/java/com/alexlasota/online_shop_order_service/OnlineShopOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexlasota/online_shop_order_service/OnlineShopOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlasota.online_shop_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
